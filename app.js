ca41f0545f314853b31c0c4e8598cbe8c86257c7
devil202 <hranjansingh@gmail.com>
2017-09-09 03:44:44 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ app.use(body.urlencoded({extended:true}));
 app.use(expressSanitiz());
 app.use(express.static('files'));
 app.use(methodOverride("_method"));
-db.connect("mongodb://localhost/Blog");
-// db.connect("mongodb://devil:himanshu@ds159033.mlab.com:59033/blogs");
+// db.connect("mongodb://localhost/Blog");
+db.connect("mongodb://devil:himanshu@ds159033.mlab.com:59033/blogs");
 
 var blogSchema=new db.Schema({
 	title:String,
 	Image:String,
 	body:String,
+	like:{type:Boolean,default:false},
+	likes:{type:Number,default:0},
 	date:{type:Date,default:Date.now}
 });
 var blog=db.model('blog',blogSchema);
@@ -40,9 +42,9 @@ app.get('/blog',function(req,res){
 });
 
 
-app.get('/blog/new',function(req,res){
-	res.render('newblog');
-});
+// app.get('/blog/new',function(req,res){
+// 	res.render('newblog');
+// });
 
 
 app.post('/blog',function(req,res){
@@ -84,53 +86,63 @@ app.get('/blog/:id',function(req,res){
 });
 
 
-app.get('/blog/:id/edit',function(req,res){
-	var id=req.params.id;
-		blog.findById(id,function(error,blog)
-		{
-			if(error)
-			{
-				console.log(error);
-			}
-			else
-			{
-				res.render('edit',{blog:blog})
-			}
-		});
-});
+// app.get('/blog/:id/edit',function(req,res){
+// 	var id=req.params.id;
+// 		blog.findById(id,function(error,blog)
+// 		{
+// 			if(error)
+// 			{
+// 				console.log(error);
+// 			}
+// 			else
+// 			{
+// 				res.render('edit',{blog:blog})
+// 			}
+// 		});
+// });
 
 
 app.put('/blog/:id',function(req,res){
 	var id=req.params.id;
-	req.body.blog.body=req.sanitize(req.body.blog.body);
-	blog.findByIdAndUpdate(id,req.body.blog,function(error,blog){
+	// req.body.blog.body=req.sanitize(req.body.blog.body);
+	// console.log(req.body.blog.body);
+	blog.findById(id,function(error,blogs){
 			if (error) 
 			{
 				console.log(error);
 			}
 			else
 			{
-				console.log(req.body.blog);
-				res.redirect('/');
+				blogs.like=!blogs.like;
+				blogs.likes+=1;
+				blog.findByIdAndUpdate(id,blogs,function(error,blog){
+					if (error) 
+					{
+						console.log(error);
+					// }
+					// else{
+							res.redirect("back");
+					}	
+				});
 			}
 		});
 });
 
 
-app.delete('/blog/:id',function(req,res){
-	var id=req.params.id;
-	blog.findByIdAndRemove(id,function(error,blog){
-			if (error) 
-			{
-				console.log(error);
-			}
-			else
-			{
-				console.log(req.body.blog);
-				res.redirect('/');
-			}
-		});
-});
+// app.delete('/blog/:id',function(req,res){
+// 	var id=req.params.id;
+// 	blog.findByIdAndRemove(id,function(error,blog){
+// 			if (error) 
+// 			{
+// 				console.log(error);
+// 			}
+// 			else
+// 			{
+// 				console.log(req.body.blog);
+// 				res.redirect('/');
+// 			}
+// 		});
+// });
 
 const port=process.env.PORT||3000;
 app.listen(port,function(){
